e8bc9baac69fffa9cf8cb16b851b518f8d1195a8
SwapnilGrespl <120772063+SwapnilGrespl@users.noreply.github.com>
2023-11-21 11:39:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [filled, setFilled] = useState(0);
+
+  const handleMouseOver = (currClicked) => {
+    const stars = document.querySelectorAll(".star");
+    stars.forEach((star, index) => {
+      if (index < currClicked) {
+        star.classList.add("star-filled");
+      } else {
+        star.classList.remove("star-filled");
+      }
+    });
+  };
+
+  const handleMouseLeave = () => {
+    const stars = document.querySelectorAll(".star");
+    stars.forEach((star, index) => {
+      if (index < filled) {
+        star.classList.add("star-filled");
+      } else {
+        star.classList.remove("star-filled");
+      }
+    });
+  };
+
+  const handleRatingClick = (e) => {
+    const currClicked = parseInt(e.target.dataset.index);
+    setFilled(currClicked);
+
+    const stars = document.querySelectorAll(".star");
+    stars.forEach((star, index) => {
+      if (index < currClicked) {
+        star.classList.add("star-filled");
+      } else {
+        star.classList.remove("star-filled");
+      }
+    });
+
+    document.getElementById("count").innerText = `Rating Count: ${currClicked}`;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Star Rating Component</h1>
+      <div
+        className="star-container"
+        id="star-container"
+        onMouseLeave={handleMouseLeave}
+      >
+        {[1, 2, 3, 4, 5].map((index) => (
+          <span
+            key={index}
+            className="star"
+            data-index={index}
+            onClick={handleRatingClick}
+            onMouseOver={() => handleMouseOver(index)}
+          >
+            &#9733;
+          </span>
+        ))}
+      </div>
+      <div id="count">Rating Count: {filled}</div>
     </div>
   );
 }
 
-export default App;
